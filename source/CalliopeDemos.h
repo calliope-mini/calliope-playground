1364043b79fa45e5bc003981db1c2ea99fb1bc1d
Matthias L. Jugel <leo@thinkberg.com>
2016-10-12 11:32:49 +0200

    add test code to compare variants

--- source/CalliopeDemos.h
+++ source/CalliopeDemos.h
@@ -1,5 +1,5 @@
 // demo and test code, uncomment a single one to play with it
 
-#define CALLIOPE_DEMO_ACCELEROMETER  1
+//#define CALLIOPE_DEMO_ACCELEROMETER  1
 //#define CALLIOPE_DEMO_SOUNDMOTORRGB  1
-//#define CALLIOPE_DEMO_RGB 1
+#define CALLIOPE_DEMO_RGB 1
