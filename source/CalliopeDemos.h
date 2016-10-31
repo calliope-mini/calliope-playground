233395c496fd937573559db429a3ac0e4e0bc531
Matthias L. Jugel <leo@thinkberg.com>
2016-10-31 09:47:11 +0100

    add special demo

--- source/CalliopeDemos.h
+++ source/CalliopeDemos.h
@@ -1,9 +1,9 @@
 // demo and test code, uncomment a single one to play with it
 
 // enable this to test the function of the board
-#define CALLIOPE_TEST_BOARD 1
-#define TEST_BUTTONS 1
-#define TEST_GESTURES 1
+//#define CALLIOPE_TEST_BOARD 1
+//#define TEST_BUTTONS 1
+//#define TEST_GESTURES 1
 
 // enable one of these for demos
 //#define CALLIOPE_DEMO_ACCELEROMETER  1
@@ -11,3 +11,4 @@
 //#define CALLIOPE_DEMO_ROBOT  1
 //#define CALLIOPE_DEMO_RGB 1
 //#define CALLIOPE_BUTTON_EVENTS_DEMO
+#define CALLIOPE_DEMO_RADIO 1
---
+++ source/CalliopeRadio.cpp
@@ -0,0 +1,52 @@
+
+#include "CalliopeDemos.h"
+#include "MicroBit.h"
+#include "nrf_delay.h"
+#include "nrf_gpio.h"
+
+
+#ifdef CALLIOPE_DEMO_RADIO
+
+MicroBit uBit;
+
+
+void buttonA(MicroBitEvent event) {
+    uBit.serial.send("START\r\n");
+    uBit.radio.datagram.send("S");
+}
+
+void buttonB(MicroBitEvent event) {
+    uBit.radio.datagram.send(uBit.random(100) < 50 ? "0" : "1");
+}
+
+void buttonAB(MicroBitEvent event) {
+    uBit.serial.send("END\r\n");
+    uBit.radio.datagram.send("E");
+}
+
+
+void onData(MicroBitEvent event) {
+    PacketBuffer packet = uBit.radio.datagram.recv();
+    uBit.serial.send("RECEIVED\r\n");
+    uBit.serial.send(packet);
+    uBit.display.print(packet);
+}
+
+int main() {
+    uBit.init();
+
+    uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, buttonA);
+    uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, buttonB);
+    uBit.messageBus.listen(MICROBIT_ID_BUTTON_AB, MICROBIT_BUTTON_EVT_CLICK, buttonAB);
+
+    uBit.messageBus.listen(MICROBIT_ID_RADIO, MICROBIT_RADIO_EVT_DATAGRAM, onData);
+
+    if(uBit.radio.enable() == MICROBIT_OK) {
+        uBit.display.scroll("Radio OK");
+    }
+    uBit.radio.setGroup(227);
+
+    while(true) uBit.sleep(100);
+}
+
+#endif
