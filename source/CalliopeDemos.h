a7a69e5ec381236a532d81338912e600dfbb80ef
Matthias L. Jugel <leo@thinkberg.com>
2017-02-16 12:19:33 +0100

    make RGB led intensity and output pin configurable

--- source/CalliopeDemos.h
+++ source/CalliopeDemos.h
@@ -1,15 +1,15 @@
 // demo and test code, uncomment a single one to play with it
 
 // enable this to test the function of the board
-#define CALLIOPE_TEST_BOARD 1
-#define TEST_BUTTONS 1
-#define TEST_GESTURES 1
+//#define CALLIOPE_TEST_BOARD 1
+//#define TEST_BUTTONS 1
+//#define TEST_GESTURES 1
 
 // enable one of these for demos
 //#define CALLIOPE_DEMO_ACCELEROMETER  1
 //#define CALLIOPE_DEMO_SOUNDMOTORRGB  1
 //#define CALLIOPE_DEMO_ROBOT  1
-//#define CALLIOPE_DEMO_RGB 1
+#define CALLIOPE_DEMO_RGB 1
 //#define CALLIOPE_BUTTON_EVENTS_DEMO
 //#define CALLIOPE_TEST_MOTORS_SINGLE 1
 //#define CALLIOPE_TEST_MOTORS_DUAL 1
